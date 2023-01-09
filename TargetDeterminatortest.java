70faf7757f45691903cbe61b8b070186c014053e
Ashwaniofficial <108612250+Ashwaniofficial@users.noreply.github.com>
2023-01-09 23:15:59 +0530

    Update TargetDeterminatortest.java

--- TargetDeterminatortest.java
+++ TargetDeterminatortest.java
@@ -1,5 +1,21 @@
 package java.paymoney;
 
 public class TargetDeterminatortest {
+	public static void main(String[] args) {
+	       
+        test(new int[] {20, 12, 31}, 19);
+        test(new int[] {100}, 101);
+        test(new int[] {20, 70, 9}, 101);  
+        test(new int[] {5, 20, 32, 12}, 57);
+        test(new int[] {5, 20, 32, 12}, 58);        
+    }
+   
+    static void test(int[] transactions, int dailyTarget) {
+       
+        System.out.println("----------------------");
+        Targetdeterminator determinator
+            = new Targetdeterminator(transactions, dailyTarget);
+        determinator.determine();
+    }
 
 }
